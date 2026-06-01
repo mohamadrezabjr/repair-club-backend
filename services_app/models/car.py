bd4933a9fe92fc01fad367f8fffdc010a54ab33d
mohamadreza <mohamadrezabjr@gmail.com>
2026-06-01 18:26:09 +0330

    refactor: separate plate number into multiple parts

--- services_app/models/car.py
+++ services_app/models/car.py
@@ -1,5 +1,6 @@
 from django.db import models
 from auth_app.models import User
+from django.core.validators import MaxValueValidator
 
 class CarModel(models.Model):
     class TransmissionTypeChoices(models.TextChoices):
@@ -15,11 +16,39 @@ class CarModel(models.Model):
         return f'{self.model}'
 
 class Car(models.Model):
+    PLATE_LETTERS = [
+        ("الف", "الف"),
+        ("ب", "ب"),
+        ("پ", "پ"),
+        ("ت", "ت"),
+        ("ث", "ث"),
+        ("ج", "ج"),
+        ("د", "د"),
+        ("س", "س"),
+        ("ص", "ص"),
+        ("ط", "ط"),
+        ("ع", "ع"),
+        ("ق", "ق"),
+        ("ل", "ل"),
+        ("م", "م"),
+        ("ن", "ن"),
+        ("و", "و"),
+        ("ه", "ه"),
+        ("ی", "ی"),
+    ]
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     model = models.ForeignKey(CarModel, on_delete=models.SET_NULL, null=True, blank=True)
-    plate_number = models.CharField(max_length=10,)
     manufacturing_date = models.IntegerField(null=True, blank=True)
     registration_date = models.DateTimeField(auto_now_add=True)
     in_garage = models.BooleanField(default=True)
     last_visit_date = models.DateTimeField(null=True, blank=True)
     last_mileage = models.IntegerField(null=True, blank=True)
+
+    plate_first = models.PositiveIntegerField(validators=[MaxValueValidator(99)])
+    plate_letter = models.CharField(choices=PLATE_LETTERS, max_length=4)
+    plate_second = models.PositiveIntegerField(validators=[MaxValueValidator(999)])
+    plate_region = models.PositiveIntegerField(validators=[MaxValueValidator(99)])
+
+    @property
+    def plate_number(self):
+        return f'{self.plate_first:02d}{self.plate_letter}{self.plate_second:03d}{self.plate_region:02d}'
