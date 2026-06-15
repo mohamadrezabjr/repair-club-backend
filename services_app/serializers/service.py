e59aebaadcbc1fd91e51e0203a2b7cf184f21a15
mohamadreza <mohamadrezabjr@gmail.com>
2026-06-15 13:48:00 +0330

    refactor: ServiceSerializer

--- services_app/serializers/service.py
+++ services_app/serializers/service.py
@@ -2,20 +2,8 @@ from rest_framework import serializers
 from services_app.models import Service
 
 
-class ServiceCreateSerializer(serializers.ModelSerializer):
-    """ Serializer for creating new services. """
-    class Meta:
-        model = Service
-        fields = [
-            'id',
-            'title',
-            'description',
-            'car_model',
-            'base_price',
-        ]
-
-class ServiceUpdateSerializer(serializers.ModelSerializer):
-    """ Serializer for updating existing services. """
+class ServiceSerializer(serializers.ModelSerializer):
+    """ Serializer for services. """
     class Meta:
         model = Service
         fields = [
--- services_app/views/service.py
+++ services_app/views/service.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from services_app.models import Service
-from services_app.serializers.service import ServiceCreateSerializer, ServiceUpdateSerializer
+from services_app.serializers.service import ServiceSerializer
 
 
 class ServiceCreateAPIView(generics.CreateAPIView):
     queryset = Service.objects.all()
     permission_classes = [IsAuthenticated, IsAdminUser]
-    serializer_class = ServiceCreateSerializer
+    serializer_class = ServiceSerializer
 
 class ServiceUpdateAPIView(generics.UpdateAPIView):
     queryset = Service.objects.all()
     permission_classes = [IsAuthenticated, IsAdminUser]
-    serializer_class = ServiceUpdateSerializer
+    serializer_class = ServiceSerializer
