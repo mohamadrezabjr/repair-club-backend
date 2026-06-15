e87924eba4bfccbb6d3a48ab55bf0a58fdf4bede
mohamadreza <mohamadrezabjr@gmail.com>
2026-06-15 17:15:59 +0330

    feat: JWT authentication by cookie

---
+++ auth_app/authentication.py
@@ -0,0 +1,28 @@
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import get_user_model
+
+class JWTAuthenticationByCookie(BaseAuthentication):
+
+    def authenticate(self, request):
+        User = get_user_model()
+        token = request.COOKIES.get("access_token")
+
+        if not token :
+            return None
+
+        try:
+            access_token = AccessToken(token)
+            user_id = access_token.get('user_id')
+            user = User.objects.get(id = user_id)
+
+        except Exception:
+            raise AuthenticationFailed("Invalid or expired token")
+
+        else :
+            token_version = access_token.get('token_version')
+            if token_version < user.token_version:
+                raise AuthenticationFailed("Invalid or expired token")
+
+        return (user, None)
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,9 +113,17 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES" : (
+        "auth_app.authentication.JWTAuthenticationByCookie",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
 }
 
-
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
