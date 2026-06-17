c5442356d9b20f94b07e5057448fe11fcb8720f7
mohamadreza <mohamadrezabjr@gmail.com>
2026-06-17 18:35:43 +0330

    feat: product_type CRUD endpoints.

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from inventory_app.views.product import ProductListAPIView, ProductRetrieveAPIView, ProductUpdateAPIView, \
-    ProductDeleteAPIView, ProductCreateAPIView
+    ProductDeleteAPIView, ProductCreateAPIView, ProductTypeRetrieveAPIView, ProductTypeCreateAPIView, \
+    ProductTypeDeleteAPIView, ProductTypeUpdateAPIView, ProductTypeListAPIView
 
 urlpatterns = [
     path('products/', ProductListAPIView.as_view(), name='product-list'),
@@ -8,4 +9,10 @@ urlpatterns = [
     path('products/<int:pk>/update/', ProductUpdateAPIView.as_view(), name='product-update'),
     path('products/<int:pk>/delete/', ProductDeleteAPIView.as_view(), name='product-delete'),
     path('products/create/', ProductCreateAPIView.as_view(), name='product-create'),
+
+    path('product_types/', ProductTypeListAPIView.as_view(), name='product-type-list'),
+    path('product_types/<int:pk>/', ProductTypeRetrieveAPIView.as_view(), name='product-type-detail'),
+    path('product_types/create/', ProductTypeCreateAPIView.as_view(), name='product-type-create'),
+    path('product_types/<int:pk>/update/', ProductTypeUpdateAPIView.as_view(), name='product-type-update'),
+    path('product_types/<int:pk>/delete/', ProductTypeDeleteAPIView.as_view(), name='product-type-delete'),
 ]
--- inventory_app/views/product.py
+++ inventory_app/views/product.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from inventory_app.models import Product
-from inventory_app.serializers.product import ProductReadSerializer, ProductWriteSerializer
+from inventory_app.models import Product, ProductType
+from inventory_app.serializers.product import ProductReadSerializer, ProductWriteSerializer, ProductTypeSerializer
 
 class ProductCreateAPIView(CreateAPIView):
     queryset = Product.objects.all().prefetch_related('product_type')
@@ -27,3 +27,25 @@ class ProductDeleteAPIView(DestroyAPIView):
     serializer_class = ProductWriteSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
 
+class ProductTypeCreateAPIView(CreateAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class ProductTypeRetrieveAPIView(RetrieveAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+
+class ProductTypeListAPIView(ListAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+
+class ProductTypeUpdateAPIView(UpdateAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class ProductTypeDeleteAPIView(DestroyAPIView):
+    queryset = ProductType.objects.all()
+    serializer_class = ProductTypeSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
