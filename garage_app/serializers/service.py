88971557b4c86117a06a5be6d7162b621ff2368b
mohamadreza <mohamadrezabjr@gmail.com>
2026-07-04 01:40:52 +0330

    refactor(garage): change fields list to "__all__"

--- garage_app/serializers/service.py
+++ garage_app/serializers/service.py
@@ -1,31 +1,22 @@
 from rest_framework import serializers
 from garage_app.models import Service, ServiceOrder
 from garage_app.serializers.car import CarModelReadSerializer
+from inventory_app.serializers.product import ProductTypeSerializer
+
 
 class ServiceWriteSerializer(serializers.ModelSerializer):
     """ Serializer for creating and updating services. """
     class Meta:
         model = Service
-        fields = [
-            'id',
-            'title',
-            'description',
-            'car_model',
-            'base_price',
-        ]
+        fields = '__all__'
 
 class ServiceReadSerializer(serializers.ModelSerializer):
     """ Serializer for retrieving service information. """
     car_model = CarModelReadSerializer(read_only=True)
+    products_needed = ProductTypeSerializer(read_only=True, many=True)
     class Meta:
         model = Service
-        fields = [
-            'id',
-            'title',
-            'description',
-            'car_model',
-            'base_price',
-        ]
+        fields = '__all__'
 
 class ServiceOrderReadSerializer(serializers.ModelSerializer):
     service = ServiceReadSerializer(read_only=True)
